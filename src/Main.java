3d9cd7ee905ed355b093deead5ad740b150fbd5a
Brian Caimmi <bcaimmi@gmail.com>
2016-05-16 10:13:00 -0300

    Primer Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Decompiled with CFR 0_114.
+ */
+public class Main {
+    public static void main(String[] args) {
+        long tiempo = 300;
+        if (args.length > 0) {
+            tiempo = Long.parseLong(args[0]);
+        }
+        WazeListener wl = new WazeListener(".\\resources", tiempo);
+        Thread t = new Thread(wl);
+        t.start();
+    }
+}
+
